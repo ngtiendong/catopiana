812ff5d40cca09f3f735a43302c5c3c43a4b800e
unamed1111 <unamed11111@gmail.com>
2019-03-25 00:12:03 +0700

    dynamic question fix

--- public/js/temp.js
+++ public/js/temp.js
@@ -17,6 +17,7 @@ let fakeAnswer = 0;
 let minLv = 1;
 let timeToChange;
 let flagChange = 0;
+let level_temp;
 
 $(document).on('click', 'label',function (event) {
     event.preventDefault();
@@ -430,9 +431,9 @@ let changeDynamicQuestion = (test_level, indexIncorrect) => {
                 if(response.question_data.length != 0){
                     Array.prototype.splice.apply(this_question.question_data, [indexIncorrect, this_question.question_data.length - indexIncorrect ].concat(response.question_data));
                     testing_data.question[position] = this_question
-                    // trừ lv khi sai 
-                    testing_data.level = test_level;
                 }
+                // trừ lv khi sai 
+                level_temp = test_level
                 // console.log('test :' )
                 // console.log(this_question)
                 
@@ -460,6 +461,10 @@ let changeDynamicQuestion = (test_level, indexIncorrect) => {
                 }
                 showTab(indexIncorrect)
                 flagChange = 0;
+                if(level_temp > minLv){
+                    console.log('vào đây dynamyc')
+                    setTimeToChange(level_temp - 1 , currentTab );
+                }
             } else {
                 console.log("error", response)
             }
@@ -486,11 +491,10 @@ let changeDynamicQuestionTimeOut = (test_level, indexIncorrect) => {
                      // ghep cau hoi vao this_question
                     Array.prototype.splice.apply(this_question.question_data, [indexIncorrect, this_question.question_data.length - indexIncorrect ].concat(response.question_data));
                     testing_data.question[position] = this_question
-                    // trừ lv khi timeout 
-                    testing_data.level = test_level;
                 }
+                 // trừ lv khi timeout 
+                    level_temp = test_level
                 // console.log(response.question_data)
-               
 
                 // total_question = parseInt(this_question.question_data.length)
                 localStorage.setItem('testing', JSON.stringify(testing_data));
@@ -512,7 +516,7 @@ function next() {
     let just_answer = $('.tab').eq(currentTab).find('input:checked')
     if (typeof $(just_answer).val() === 'undefined'){
         //Chua tra loi
-        console.log(tab_number[currentTab], currentTab, tab_number, just_answer, just_answer.val())
+        // console.log(tab_number[currentTab], currentTab, tab_number, just_answer, just_answer.val())
         alert("Please answer the question")
     } else {
         // dừng việc check20s tránh đuplicate lặp timeout
@@ -532,10 +536,14 @@ function next() {
             showTab(currentTab);
         } else {
             // compare answer
-            console.log(flagChange)
-            if(just_answer != fakeAnswer && parseInt(testing_data.level) > minLv && flagChange == 0 ){
+            console.log(flagChange) 
+            console.log(level_temp)
+            if(typeof(level_temp) === 'undefined'){
+                level_temp = parseInt(testing_data.level);
+            }
+            if(just_answer != fakeAnswer && level_temp > minLv && flagChange == 0 ){
                 console.log('w answer');
-                changeDynamicQuestion(parseInt(testing_data.level) -1 , this_question.answers.length)
+                changeDynamicQuestion(level_temp -1 , this_question.answers.length)
             } else {
                 //Save into local storage
                 // console.log (testing_data, 'test')
@@ -560,9 +568,10 @@ function next() {
                         hideQuestion(currentTab);
                     }
                 showTab(currentTab)
+                flagChange = 0 
                 // sau khi render 20s k next thì sẽ => câu hỏi thấp
-                if(parseInt(testing_data.level) > minLv){
-                    setTimeToChange(testing_data.level - 1 , currentTab );
+                if(level_temp > minLv){
+                    setTimeToChange(level_temp - 1 , currentTab );
                 }
             }
         }
