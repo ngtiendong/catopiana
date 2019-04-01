4b53585c87dfd3fd695c8ae24577bbf5be0e3baa
unamed1111 <unamed11111@gmail.com>
2019-04-01 23:05:20 +0700

    merrge masster

--- Modules/Frontend/Resources/views/layouts/script.blade.php
+++ Modules/Frontend/Resources/views/layouts/script.blade.php
@@ -19,6 +19,3 @@
 </script>
 <!-- Some function need to run in document ready function: Select2 ,.... -->
 <script src="{{ asset('helperJS/documentReadyFunction.js') }}"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-@stack('js')
