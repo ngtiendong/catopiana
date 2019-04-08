a1c46e3e846aa44c88c43894641947f68c4b3a22
unamed1111 <unamed11111@gmail.com>
2019-04-08 13:02:52 +0700

    data fake

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $index = 0;
+        for($i = 1; $i < 41 ; $i++)
+        {
+        		$index++;
+        		if($index == 7) $index = 1;
+	        	 DB::table('questions')->insert([
+	        	'curriculum_id' => 1,
+	        	'index' => $index,
+	        	'question' => 'afterInit/1.png',
+	        	'correct_answer' => 'afterInit/1.png',
+	        	'wrong_answer' => json_encode(['afteInit/1a.png','afteInit/1b.png','afteInit/1c.png']),
+	        ]);
+        }
+       
     }
 }
