2d25c12baca17755cb7c925e3b579429a6bbf282
unamed1111 <unamed11111@gmail.com>
2019-04-21 12:23:06 +0700

    fix giao dien

--- Modules/Frontend/Resources/views/layouts/layout.blade.php
+++ Modules/Frontend/Resources/views/layouts/layout.blade.php
@@ -11,22 +11,6 @@
 			@include('frontend::layouts.footer')
 		</div>
 		@include('frontend::layouts.script')
-
-
-		<div id="lbdictex_find_popup" class="lbexpopup hidden" style="position: absolute; top: 0px; left: 0px;">
-			<div class="lbexpopup_top">
-				<h2 class="fl popup_title">&nbsp;</h2>
-				<ul>
-					<li><a class="close_main popup_close" href="#">&nbsp;</a></li>
-				</ul>
-				<div class="clr"></div>
-			</div>
-			<div class="popup_details"></div>
-			<div class="popup_powered">abc</div>
-		</div>
-		<div id="lbdictex_ask_mark" class="hidden" style="position: absolute; top: 0px; left: 0px;">
-			<a class="lbdictex_ask_select" href="#">&nbsp;</a>
-		</div>
 	</body>
 </html>
 
