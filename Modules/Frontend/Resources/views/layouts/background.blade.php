136947676c5f44baf1ed26da2c629bfe8191ebc1
dongnat <tung91996@gmail.com>
2019-05-09 00:20:27 +0700

    updates

--- Modules/Frontend/Resources/views/layouts/background.blade.php
+++ Modules/Frontend/Resources/views/layouts/background.blade.php
@@ -7,7 +7,17 @@
 <img class="b7 bubble" src="{{asset('./Catopiana_files/images/b1.svg')}}" alt="">
 <img class="b8 bubble" src="{{asset('./Catopiana_files/images/b1.svg')}}" alt="">
 <img class="b9 bubble" src="{{asset('./Catopiana_files/images/b1.svg')}}" alt="">
-<img class="b10 bubble" src="{{asset('./Catopiana_files/images/b1.svg')}}" alt="">
+{{--<img class="b10 bubble-top bubble" src="{{asset('./Catopiana_files/images/b1.svg')}}" alt="" style="left: 20%">--}}
+<img class="b11 bubble-top bubble" src="{{asset('./Catopiana_files/images/b1.svg')}}" alt=""  style="left: 25%">
+<img class="b12 bubble-top bubble" src="{{asset('./Catopiana_files/images/b1.svg')}}" alt=""  style="left: 44%">
+<img class="b13 bubble-top bubble" src="{{asset('./Catopiana_files/images/b1.svg')}}" alt=""  style="left: 34%">
+<img class="b14 bubble-top bubble" src="{{asset('./Catopiana_files/images/b1.svg')}}" alt=""  style="left: 12%">
+<img class="b15 bubble-top bubble" src="{{asset('./Catopiana_files/images/b1.svg')}}" alt=""  style="left: 78%">
+<img class="b16 bubble-top bubble" src="{{asset('./Catopiana_files/images/b1.svg')}}" alt="" style="left: 86%">
+<img class="b17 bubble-top bubble" src="{{asset('./Catopiana_files/images/b1.svg')}}" alt="" style="left: 45%">
+<img class="b18 bubble-top bubble" src="{{asset('./Catopiana_files/images/b1.svg')}}" alt="" style="left: 25%">
+<img class="b19 bubble-top bubble" src="{{asset('./Catopiana_files/images/b1.svg')}}" alt="" style="left: 91%">
+<img class="b20 bubble-top bubble" src="{{asset('./Catopiana_files/images/b1.svg')}}" alt="" style="left: 80%">
 <img class="f6 fish" src="{{asset('./Catopiana_files/images/f6.svg')}}" alt="">
 <img class="f7 fish" src="{{asset('./Catopiana_files/images/f7.svg')}}" alt="">
 <img class="f1 fish" src="{{asset('./Catopiana_files/images/f1.svg')}}" alt="">
