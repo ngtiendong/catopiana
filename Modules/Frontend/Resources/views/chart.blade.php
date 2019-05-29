6798d0eeb5bcef9ae7bdba6e8cf0543a105742b8
tudm-0884 <dao.minh.tu@sun-asterisk.com>
2019-05-29 23:16:35 +0700

    mobile chart-bladde

--- Modules/Frontend/Resources/views/chart.blade.php
+++ Modules/Frontend/Resources/views/chart.blade.php
@@ -8,6 +8,7 @@
         </div>
         <div class="chart-wrapper">
             <canvas id="chart"></canvas>
+            <div id="legend"></div>
         </div>
         <div class="button-chart-wr text-center">
             <button type="button" class="button-chart gradient-button">Menu for 30 day</button>
@@ -23,44 +24,6 @@
 <script type="text/javascript" src="{{asset('/js/menu.js')}}"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels"></script>
+<script type="text/javascript" src="{{asset('/js/chart.js')}}"></script>
 
-<script>
-    let ctx = document.getElementById('chart').getContext('2d');
-    let labels = ['Protein', 'Fat', 'Cacbonhydrate'];
-    let color = ['rgba(255, 99, 132)', 'rgba(255, 206, 86)', 'rgba(75, 192, 192)'];
-
-    let chart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-            datasets: [{
-                data: [20, 20, 60],
-                backgroundColor: color,
-                borderWidth: 0,
-            }],
-            labels: labels
-        },
-        options: {
-            reponsive: true,
-            legend: {
-                position: 'bottom',
-                labels: {
-                    fontColor: 'white'
-                },
-                fullWidth: false,
-            },
-            plugins: {
-                datalabels: {
-                    color: 'black',
-                    font: {
-                        size: '30',
-                        weight: 'bold'
-                    },
-                    formatter: (value) => {
-                        return value + '%';
-                    }
-                },
-            }
-        }
-    })
-</script>
 @endsection
