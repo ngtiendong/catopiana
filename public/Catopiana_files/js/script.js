7b3d7f128749da1e4f2869e904cb69a3bf3b8d88
dongnat <tung91996@gmail.com>
2019-06-02 17:12:32 +0700

    updates monitor

--- public/Catopiana_files/js/script.js
+++ public/Catopiana_files/js/script.js
@@ -167,18 +167,20 @@ jQuery(function($) {
 		tl2.pause();
 	});
 
+	if (window.width() > 1024) {
+		$($('.test-item')).hover(function() {
+			tlgt = new TimelineMax();
+			tlgt.to($(this), 0.05, {scale: 1.3}, 0.1);
+			tlgt1 = new TimelineMax({ repeat: -1, yoyo: true });
+			tlgt1.fromTo($(this), 0.05, {rotation:-10, transformOrigin:"50% 50%"}, {rotation:10, transformOrigin:"50% 50%"}, 0.1);
+		}, function() {
+			tlgt.restart();
+			tlgt.pause();
+			tlgt1.restart();
+			tlgt1.pause();
+		});
+	}
 
-	$($('.test-item')).hover(function() {
-		tlgt = new TimelineMax();
-		tlgt.to($(this), 0.05, {scale: 1.3}, 0.1);
-		tlgt1 = new TimelineMax({ repeat: -1, yoyo: true });
-		tlgt1.fromTo($(this), 0.05, {rotation:-10, transformOrigin:"50% 50%"}, {rotation:10, transformOrigin:"50% 50%"}, 0.1);
-	}, function() {
-		tlgt.restart();
-		tlgt.pause();
-		tlgt1.restart();
-		tlgt1.pause();
-	});
 
 	// end animation
 
